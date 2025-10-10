92a1b4fb8b3bcf2d3509b3fc5757030aa2bc924f
Krotkeros <143474446+Krotkeros@users.noreply.github.com>
2025-10-09 19:48:01 -0500

    Querys implements e interfacaes

--- QueBox.Query/Implements/DisenoQueries.cs
+++ QueBox.Query/Implements/DisenoQueries.cs
@@ -1,36 +1,115 @@
-﻿using Dapper;
-using MiApi.Models;
-using MiApi.Query.Interfaces;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using QueBox.Query.Interfaces;
 
-namespace MiApi.Query.Implements
+namespace QueBox.Query
 {
-    public class PersonaQueries : IPersonaQueries
+    public class DisenoQueries : IDisenoQueries
     {
-        private readonly IDbConnection _db;
+        private readonly IDbConnection _connection;
 
-        public PersonaQueries(IDbConnection db)
+        public DisenoQueries(IDbConnection connection)
         {
-            _db = db ?? throw new ArgumentNullException(nameof(db));
+            _connection = connection;
         }
 
-        public async Task<IEnumerable<Persona>> GetAll()
+        public async Task<Diseno> ObtenerPorIdAsync(int id)
         {
-            try
-            {
-                string sql = "SELECT * FROM Personas";
-                var rs = await _db.QueryAsync<Persona>(sql);
-                return rs;
-            }
-            catch (Exception)
-            {
+            const string query = @"
+                SELECT ID_diseno, ID_usuario, ID_Cara, Largo, Alto, Ancho, Nombre
+                FROM Diseno
+                WHERE ID_diseno = @Id";
 
-                throw;
-            }
+            return await _connection.QueryFirstOrDefaultAsync<Diseno>(query, new { Id = id });
+        }
+
+        public async Task<IEnumerable<Diseno>> ObtenerPorUsuarioAsync(int idUsuario)
+        {
+            const string query = @"
+                SELECT ID_diseno, ID_usuario, ID_Cara, Largo, Alto, Ancho, Nombre
+                FROM Diseno
+                WHERE ID_usuario = @IdUsuario
+                ORDER BY ID_diseno DESC";
+
+            return await _connection.QueryAsync<Diseno>(query, new { IdUsuario = idUsuario });
+        }
+
+        public async Task<IEnumerable<Diseno>> ObtenerPorCaraAsync(int idCara)
+        {
+            const string query = @"
+                SELECT ID_diseno, ID_usuario, ID_Cara, Largo, Alto, Ancho, Nombre
+                FROM Diseno
+                WHERE ID_Cara = @IdCara
+                ORDER BY ID_diseno DESC";
+
+            return await _connection.QueryAsync<Diseno>(query, new { IdCara = idCara });
+        }
+
+        public async Task<IEnumerable<Diseno>> ObtenerTodosAsync()
+        {
+            const string query = @"
+                SELECT ID_diseno, ID_usuario, ID_Cara, Largo, Alto, Ancho, Nombre
+                FROM Diseno
+                ORDER BY ID_diseno DESC";
+
+            return await _connection.QueryAsync<Diseno>(query);
+        }
+
+        public async Task<Diseno> ObtenerDisenoCompletoAsync(int id)
+        {
+            const string query = @"
+                SELECT 
+                    d.ID_diseno, d.ID_usuario, d.ID_Cara, d.Largo, d.Alto, d.Ancho, d.Nombre,
+                    u.ID_usuario, u.Usuario, u.Correo,
+                    c.ID_Cara, c.ID_IMG, c.Numero
+                FROM Diseno d
+                INNER JOIN Usuario u ON d.ID_usuario = u.ID_usuario
+                INNER JOIN Cara c ON d.ID_Cara = c.ID_Cara
+                WHERE d.ID_diseno = @Id";
+
+            var disenoDictionary = new Dictionary<int, Diseno>();
+
+            var result = await _connection.QueryAsync<Diseno, Usuario, Cara, Diseno>(
+                query,
+                (diseno, usuario, cara) =>
+                {
+                    if (!disenoDictionary.TryGetValue(diseno.ID_diseno, out var disenoEntry))
+                    {
+                        disenoEntry = diseno;
+                        disenoEntry.Usuario = usuario;
+                        disenoEntry.Cara = cara;
+                        disenoDictionary.Add(disenoEntry.ID_diseno, disenoEntry);
+                    }
+                    return disenoEntry;
+                },
+                new { Id = id },
+                splitOn: "ID_usuario,ID_Cara"
+            );
+
+            return result.FirstOrDefault();
+        }
+
+        public async Task<int> ContarDisenoPorUsuarioAsync(int idUsuario)
+        {
+            const string query = @"
+                SELECT COUNT(*)
+                FROM Diseno
+                WHERE ID_usuario = @IdUsuario";
+
+            return await _connection.ExecuteScalarAsync<int>(query, new { IdUsuario = idUsuario });
+        }
+
+        public async Task<IEnumerable<Diseno>> ObtenerDisenosRecientesAsync(int limite)
+        {
+            const string query = @"
+                SELECT TOP(@Limite) ID_diseno, ID_usuario, ID_Cara, Largo, Alto, Ancho, Nombre
+                FROM Diseno
+                ORDER BY ID_diseno DESC";
+
+            return await _connection.QueryAsync<Diseno>(query, new { Limite = limite });
         }
     }
 }
--- QueBox.Query/Implements/ImagenDecorativaQueries.cs
+++ QueBox.Query/Implements/ImagenDecorativaQueries.cs
@@ -1,36 +1,106 @@
-﻿using Dapper;
-using MiApi.Models;
-using MiApi.Query.Interfaces;
-using System;
+﻿
 using System.Collections.Generic;
 using System.Data;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using QueBox.Query.Interfaces;
 
-namespace MiApi.Query.Implements
+namespace QueBox.Query
 {
-    public class PersonaQueries : IPersonaQueries
+    public class ImagenDecorativaQueries : IImagenDecorativaQueries
     {
-        private readonly IDbConnection _db;
+        private readonly IDbConnection _connection;
 
-        public PersonaQueries(IDbConnection db)
+        public ImagenDecorativaQueries(IDbConnection connection)
         {
-            _db = db ?? throw new ArgumentNullException(nameof(db));
+            _connection = connection;
         }
 
-        public async Task<IEnumerable<Persona>> GetAll()
+        public async Task<ImagenDecorativa> ObtenerPorIdAsync(int id)
         {
-            try
-            {
-                string sql = "SELECT * FROM Personas";
-                var rs = await _db.QueryAsync<Persona>(sql);
-                return rs;
-            }
-            catch (Exception)
-            {
+            const string query = @"
+                SELECT ID_IMG, Url, Ancho, Alto
+                FROM Imagen_Decorativa
+                WHERE ID_IMG = @Id";
 
-                throw;
-            }
+            return await _connection.QueryFirstOrDefaultAsync<ImagenDecorativa>(query, new { Id = id });
+        }
+
+        public async Task<IEnumerable<ImagenDecorativa>> ObtenerTodasAsync()
+        {
+            const string query = @"
+                SELECT ID_IMG, Url, Ancho, Alto
+                FROM Imagen_Decorativa
+                ORDER BY ID_IMG";
+
+            return await _connection.QueryAsync<ImagenDecorativa>(query);
+        }
+
+        public async Task<ImagenDecorativa> ObtenerPorUrlAsync(string url)
+        {
+            const string query = @"
+                SELECT ID_IMG, Url, Ancho, Alto
+                FROM Imagen_Decorativa
+                WHERE Url = @Url";
+
+            return await _connection.QueryFirstOrDefaultAsync<ImagenDecorativa>(query, new { Url = url });
+        }
+
+        public async Task<IEnumerable<ImagenDecorativa>> ObtenerPorDimensionesAsync(float largo, float alto)
+        {
+            const string query = @"
+                SELECT ID_IMG, Url, Ancho, Alto
+                FROM Imagen_Decorativa
+                WHERE Ancho = @Largo AND Alto = @Alto";
+
+            return await _connection.QueryAsync<ImagenDecorativa>(query, new { Largo = largo, Alto = alto });
+        }
+
+        public async Task<IEnumerable<ImagenDecorativa>> ObtenerImagenesConCarasAsync()
+        {
+            const string query = @"
+                SELECT 
+                    i.ID_IMG, i.Url, i.Ancho, i.Alto,
+                    c.ID_Cara, c.ID_IMG, c.Numero
+                FROM Imagen_Decorativa i
+                LEFT JOIN Cara c ON i.ID_IMG = c.ID_IMG
+                ORDER BY i.ID_IMG, c.Numero";
+
+            var imagenDictionary = new Dictionary<int, ImagenDecorativa>();
+
+            var result = await _connection.QueryAsync<ImagenDecorativa, Cara, ImagenDecorativa>(
+                query,
+                (imagen, cara) =>
+                {
+                    if (!imagenDictionary.TryGetValue(imagen.ID_IMG, out var imagenEntry))
+                    {
+                        imagenEntry = imagen;
+                        imagenEntry.Caras = new List<Cara>();
+                        imagenDictionary.Add(imagenEntry.ID_IMG, imagenEntry);
+                    }
+
+                    if (cara != null)
+                    {
+                        imagenEntry.Caras.Add(cara);
+                    }
+
+                    return imagenEntry;
+                },
+                splitOn: "ID_Cara"
+            );
+
+            return imagenDictionary.Values;
+        }
+
+        public async Task<int> ContarCarasPorImagenAsync(int idImagen)
+        {
+            const string query = @"
+                SELECT COUNT(*)
+                FROM Cara
+                WHERE ID_IMG = @IdImagen";
+
+            return await _connection.ExecuteScalarAsync<int>(query, new { IdImagen = idImagen });
         }
     }
 }
